6c6a70100cc9dc1d5dee3b264753bb9f0f014182
Guwel <guwel@yandex.ru>
2022-12-28 15:54:27 +0500

    add task

---
+++ Task/Program.cs
@@ -0,0 +1,29 @@
+﻿
+void Array3Symbols(string[] cities, string[] cities2)
+{
+    int count = 0;
+    for (int i = 0; i < cities.Length; i++)
+    {
+    if(cities[i].Length <= 3)
+        {
+        cities2[count] = cities[i];
+        count++;
+        }
+    }
+}
+void PrintArray(string[] cities)
+{
+    for (int i = 0; i < cities.Length; i++)
+    {
+        Console.Write($"{cities[i]}, ");
+    }
+    Console.WriteLine();
+}
+
+
+Console.Clear();
+
+string[] cities = { "Bam", "Bra", "Ami", "Moskow" };
+string[] cities2 = new string[cities.Length];
+Array3Symbols(cities, cities2);
+PrintArray(cities2);
